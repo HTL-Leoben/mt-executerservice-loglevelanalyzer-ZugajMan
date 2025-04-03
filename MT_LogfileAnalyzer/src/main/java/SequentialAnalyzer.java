4ea3177ac8dc73b4b455dfadaf2c252dac000b27
ZugajMan <230077@O365.htl-leoben.at>
2025-04-03 09:58:07 +0200

    first commit of project

---
+++ MT_LogfileAnalyzer/src/main/java/SequentialAnalyzer.java
@@ -0,0 +1,13 @@
+import tools.LogGenerator;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class SequentialAnalyzer {
+    LogGenerator logGenerator;
+
+
+    private String logFilePath= "";
+    BufferedReader logFileReader = new BufferedReader(new FileReader(logFilePath));
+
+}
